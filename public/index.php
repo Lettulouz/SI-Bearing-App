3a7e4f9920ea07430e6986adbb49ec1e42c1fbf1
Lettulouz <dominikpiskor1@wp.pl>
2022-11-24 21:27:41 +0100

    Project recovery

--- public/index.php
+++ public/index.php
@@ -1,50 +1,6 @@
-<html>
-<head>
-<meta charset="utf8" />
-<title> ananasowy zamek </title>
-<link rel="stylesheet" href="css/styles.css" />
-</head>
-<body>
-<div id="naglowek">
-        
-        <div id="naglowek1">
-        test nagłówku 1
-        </div>
-
-        <div id="naglowek2">
-                <a href="index.php?logout=true"> Wyloguj</a> &nbsp
-                <a href="index.php?action=wyswietl"> Wyswietl </a> &nbsp
-                <a href="index.php?action=dodaj"> Dodaj </a> &nbsp
-                <a href="index.php?action=ksiazki"> ksiazki </a>
-        </div>
-</div>
-
-<div id="wnetrze">
-        <div id="wnetrze1">
-                Tu jest wnętrze <br/>
-                <hr/>
-                <p> test </p>
-                <b> ananas </b>
-        </div>
-
-        <div id="wnetrze2">
-                Tu jest wnętrze 2
-        </div>
-
-</div>
-
-
-</body>
-<html>
-
-
-
-
-
-
 <?php
 
-//require_once dirname(__FILE__,2) . '/app/init.php';
-//session_start();
-//$app = new App;
+require_once dirname(__FILE__,2) . '/app/init.php';
+session_start();
+$app = new App;
 ?>
