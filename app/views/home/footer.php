833aa33bbec3b9fb7e199ba079c5321c5d87c1f7
Kornwaliusz <wojciech.j.a.stachura@gmail.com>
2022-11-24 20:52:57 +0100

    wysylam jeszcze raz

--- app/views/home/footer.php
+++ app/views/home/footer.php
@@ -1,2 +1,14 @@
+<hr/>
+                <p> test </p>
+                <b> ananas </b>
+        </div>
+
+        <div id="wnetrze2">
+                Tu jest wnętrze 2
+        </div>
+
+</div>
+
+
 </body>
-</html>
+<html>
--- app/views/home/header.php
+++ app/views/home/header.php
@@ -1,11 +1,32 @@
+                  
 <html>
-    <head> 
+<head>
+    <title>PHP</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?=CSSPATH?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div id="naglowek">
         
-        <title>PHP</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="<?=CSSPATH?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <div id="naglowek1">
+        test nagłówku 1
+        </div>
 
-    </head>
-    <body>
-                      
+        <div id="naglowek2">
+                <a href="index.php?logout=true"> Wyloguj</a> &nbsp
+                <a href="index.php?action=wyswietl"> Wyswietl </a> &nbsp
+                <a href="index.php?action=dodaj"> Dodaj </a> &nbsp
+                <a href="index.php?action=ksiazki"> ksiazki </a>
+        </div>
+</div>
+
+<div id="wnetrze">
+        <div id="wnetrze1">
+                Tu jest wnętrze <br/>
+                <hr/>
+                <p> test </p>
+                <b> ananas </b>
+        </div>
+
+        <div id="wnetrze2">
