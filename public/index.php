1dbe3febe23879c864f8678b09c24c3b094ea282
Kornwaliusz <wojciech.j.a.stachura@gmail.com>
2022-11-24 19:17:00 +0100

    css test

--- public/index.php
+++ public/index.php
@@ -1,6 +1,26 @@
+<html>
+<head>
+<meta charset="utf8" />
+<title> ananasowy zamek </title>
+<link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+
+
+
+
+
+<body>
+<html>
+
+
+
+
+
+
 <?php
 
-require_once dirname(__FILE__,2) . '/app/init.php';
-session_start();
-$app = new App;
+//require_once dirname(__FILE__,2) . '/app/init.php';
+//session_start();
+//$app = new App;
 ?>
