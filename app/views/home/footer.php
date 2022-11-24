dfb2acfd2e5bb1ff01720a30b681ae40877f822e
Lettulouz <dominikpiskor1@wp.pl>
2022-11-24 21:29:39 +0100

    Project recovery

--- app/views/home/footer.php
+++ app/views/home/footer.php
@@ -1,14 +1,2 @@
-<hr/>
-                <p> test </p>
-                <b> ananas </b>
-        </div>
-
-        <div id="wnetrze2">
-                Tu jest wnętrze 2
-        </div>
-
-</div>
-
-
 </body>
 <html>
--- app/views/home/header.php
+++ app/views/home/header.php
@@ -1,32 +1,10 @@
-                  
 <html>
-<head>
-    <title>PHP</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?=CSSPATH?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-<div id="naglowek">
-        
-        <div id="naglowek1">
-        test nagłówku 1
-        </div>
+    <head> 
 
-        <div id="naglowek2">
-                <a href="index.php?logout=true"> Wyloguj</a> &nbsp
-                <a href="index.php?action=wyswietl"> Wyswietl </a> &nbsp
-                <a href="index.php?action=dodaj"> Dodaj </a> &nbsp
-                <a href="index.php?action=ksiazki"> ksiazki </a>
-        </div>
-</div>
+        <title>PHP</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="<?=CSSPATH?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<div id="wnetrze">
-        <div id="wnetrze1">
-                Tu jest wnętrze <br/>
-                <hr/>
-                <p> test </p>
-                <b> ananas </b>
-        </div>
-
-        <div id="wnetrze2">
+    </head>
+    <body>
