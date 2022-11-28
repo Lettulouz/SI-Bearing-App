ec2969b50010ea3555f318024db580696c397526
Kornwaliusz <wojciech.j.a.stachura@gmail.com>
2022-11-28 22:16:05 +0100

    pliki do logowania

---
+++ app/views/logowanie/header.php
@@ -0,0 +1,11 @@
+<html>
+    <head> 
+
+        <title>PHP</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="<?=CSSPATH?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    </head>
+    <body>
+    
