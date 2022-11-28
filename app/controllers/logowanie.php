580a24dea5e3a225359e2c85db4b82816fa76b4b
Kornwaliusz <wojciech.j.a.stachura@gmail.com>
2022-11-28 23:11:25 +0100

    prawie dzialajoce logowanie

--- app/controllers/logowanie.php
+++ app/controllers/logowanie.php
@@ -1,9 +1,21 @@
 <?php
 class Logowanie extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $message = "czas się zalogować";
         $this->view('logowanie/index', ['message' => $message]);
+    }
+
+    public function test()
+    {
+        $message = "test";
+        $this->view('logowanie/index', ['message' => $message]);
+    }
+
+    public function test2($login = '', $haslo = '')
+    {
+        $this->view('logowanie/index', ['login' => $login, 'haslo' => $haslo]);
     }
 }
 ?>
--- app/views/logowanie/index.php
+++ app/views/logowanie/index.php
@@ -3,21 +3,28 @@
     <h1>logowanie</h1>
 
         <?php
-			if(isset($_POST['Login']))
-				$Login= $_POST['Login'];
-			else $Login='wartosc domyslna';
+        
+        /*
+			if(isset($_POST['login']))
+				$login= $_POST['login'];
+			else $login='';
+
+			if(isset($_POST['haslo']))
+				$haslo= $_POST['haslo'];
+			else $haslo='';
+            <?=$data['message']?>
+
+           */ 
 		?>
 
-        <?php
-			if(isset($_POST['Hasło']))
-				$Hasło= $_POST['Hasło'];
-			else $Hasło='wartosc domyslna';
-		?>
+
+    <?=$data['login']?>
+    <?=$data['haslo']?>
 
 
-    <form method="post" action="index.php">
-			<input type="text" name="Login" value="<?php echo $Login;?>"></br>
-            <input type="text" name="Hasło" value="<?php echo $Hasło;?>"></br>
+    <form method="post" action="/si-project-php/public/logowanie/test2/.$login.$haslo ">
+			login: <input type="text" name="login" /></br> <br/>
+            hasło: <input type="text" name="haslo" /></br> <br/>
 			<input type="submit" value="Zaloguj">
 		</form>
 
