314c325fdf434c49691ce4e4445b4ba408d4935e
Arek2017 <agront@interia.pl>
2022-11-28 22:36:47 +0100

    Logowanie1

--- app/views/logowanie/index.php
+++ app/views/logowanie/index.php
@@ -1,7 +1,25 @@
 <?php include "header.php"; ?>
 
     <h1>logowanie</h1>
-    <br>
-    <?=$data['message']?>
+
+        <?php
+			if(isset($_POST['Login']))
+				$Login= $_POST['Login'];
+			else $Login='wartosc domyslna';
+		?>
+
+        <?php
+			if(isset($_POST['Hasło']))
+				$Hasło= $_POST['Hasło'];
+			else $Hasło='wartosc domyslna';
+		?>
+
+
+    <form method="post" action="index.php">
+			<input type="text" name="Login" value="<?php echo $Login;?>"></br>
+            <input type="text" name="Hasło" value="<?php echo $Hasło;?>"></br>
+			<input type="submit" value="Zaloguj">
+		</form>
+
 
 <?php include dirname(__FILE__,2) . "/footer.php"; ?>
