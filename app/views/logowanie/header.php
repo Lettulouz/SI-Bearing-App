630138ee3a6d9dfe74e69e22df570a72258b1c5f
Kornwaliusz <wojciech.j.a.stachura@gmail.com>
2022-11-28 22:29:45 +0100

    tytul strony

--- app/views/logowanie/header.php
+++ app/views/logowanie/header.php
@@ -1,7 +1,7 @@
 <html>
     <head> 
 
-        <title>PHP</title>
+        <title>logowanie</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="<?=CSSPATH?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
