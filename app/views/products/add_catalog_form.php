3eaf39cdb8daaef4c2355215908e0ec96f0dfb89
tentegess <tenagent2222@gmail.com>
2022-12-06 00:29:43 +0100

    Dodawanie katalogów (prawie działa)

--- app/views/products/add_catalog_form.php
+++ app/views/products/add_catalog_form.php
@@ -7,24 +7,24 @@
 <h1 class="text-muted">Dodawanie katalogu</h1>
     <hr class="divider ">
 <div class="container" style="max-width:720px;">
-    
-    <form>
+    <!-- nie działa dla menedżera contentu jak coś-->
+    <form method="post"  action="">
         <div class="row m-2">
             <div class="col-12 ">
                 <div class="row m-2">
                     <div class="col-12">
                         <div class="form-floating">
-                            <input type="text" class="form-control" id="nameInput" name="name" placeholder="Grontex">
+                            <input type="text" class="form-control" id="nameInput" name="catname" placeholder="Grontex">
                             <label for="nameInput" >Nazwa katalogu</label>
                         </div>
                     </div>
                 </div>
                 <div class="row m-2">
                     <div class="col-12">
-                        <select class="select2 form-select " multiple="multiple" id="item1" name="item1" aria-label="example">
+                        <select class="select2 form-select " multiple="multiple" id="item" name="itemcat[]" aria-label="example">
                                 <?php
-                                    foreach($data as $i => $result) {
-                                        echo "<option value=".$result['id'].">".$result['name']."</option>";
+                                    foreach($data['items'] as $i => $result) {
+                                        echo "<option value=".$result['item_id'].">".$result['mnf']." - ".$result['item']."</option>";
 
                                     }
                                 
@@ -32,11 +32,13 @@
                         </select>
 
                     </div>
+                    <span style="color:<?php if(isset($_POST['catsubmit'])) echo $data['msg_color']; ?>">
+                    <?php if(isset($_POST['catsubmit'])) echo $data['return_msg']; ?></span>
                 </div>
                            
                 <div class="row m-2">
                     <div class="float-end">
-                        <button type="submit" class="btn btn-primary btn-lg float-end">Dodaj</button>
+                        <button type="submit" name="catsubmit" class="btn btn-primary btn-lg float-end">Dodaj</button>
                     </div>
 
             </div>
@@ -50,7 +52,7 @@ document.getElementById('content_collapse').classList.add('show');
 document.getElementById('content_collapse_btn').setAttribute('aria-expanded', 'true');
 document.getElementById('content_collapse_btn').setAttribute('style', 'color:white !important');
 document.getElementById('addcat').setAttribute('style', 'color:white !important');
-$('#item1').select2({
+$('#item').select2({
     theme: 'bootstrap-5',
     placeholder: 'Wybierz produkty'
 });
