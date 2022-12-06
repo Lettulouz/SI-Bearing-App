3d8f2b685405270eb95d3bb89e3d8ea4ee84a592
Kornwaliusz <wojciech.j.a.stachura@gmail.com>
2022-12-06 23:03:52 +0100

    attribut list

--- app/views/products/attributes_list.php
+++ app/views/products/attributes_list.php
@@ -1,4 +1,33 @@
-<p>lista atrybutów</p>
+
+<h1 class="text-muted headers-padding">Lista atrybutów</h1>
+    <hr class="divider ">
+    <div class="headers-padding" style="padding-right: 15px;">
+        <?php if($data['attributesArray']) {?>
+        <table class="table text-center">
+        <thead>
+        <tr>
+        <th>Lp.</th>
+        <th>Nazwa</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php 
+        $attribut = $data['attributesArray'];
+        $i = 1;
+        foreach($attribut as $attribut) 
+        {
+            echo 
+            "<tr>
+            <td>{$i}</td>
+            <td>{$attribut['name']}</td>
+            </tr>";
+            $i++;
+        }
+        ?>
+        </tbody>
+        </table>
+        <?php } else {echo "Brak dodanych atrybutów.";}?>
+    </div>
 
 <script>
     document.getElementById('content_collapse').classList.add('show');
