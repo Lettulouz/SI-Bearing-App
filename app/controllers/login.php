ae543923c0f6bf0252d026f936d7600ed4438257
Lettulouz <dominikpiskor1@wp.pl>
2022-12-06 11:48:38 +0100

    First connection of login page with database

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -12,23 +12,34 @@ class Login extends Controller
         $login = "admin";
         $password = "admin123";
         $check = 1;
+        $emailOrLoginCheck = 0;
         $serverError = false;
         echo "<script src='" . APPPATH . "/scripts/login.js" .  "'></script>";
+
         if(isset($_SESSION['emailOrLoginInput'])){
             $emailOrLoginInput = $_SESSION['emailOrLoginInput'];
         }else{
             $emailOrLoginInput = "";
         }
         $errorPassword = "";
+        $ifUserExist = "";
 
         if(isset($_POST['emailOrLogin'])){
+            require_once dirname(__FILE__,2) . '/core/database.php';
+
             $emailOrLoginInput = strtolower($_POST['emailOrLogin']);
             $passwordInput = $_POST['password'];
 
             if(str_contains($emailOrLoginInput, '@')){
                 if($this->verifyEmail($emailOrLoginInput)){
                     $_SESSION['emailOrLoginInput'] =  $emailOrLoginInput;
-                    if($emailOrLoginInput != $login){
+                    $userQuery = $db->prepare('SELECT id, password FROM users WHERE email = :email');
+                    $userQuery->bindValue(':email', $emailOrLoginInput, PDO::PARAM_STR);
+                    $userQuery->execute();
+
+                    $ifUserExist = $userQuery->fetch(PDO::FETCH_ASSOC);
+
+                    if(!$ifUserExist){
                         $errorPassword = "*Podano błędny email lub hasło";
                         $_SESSION['errorPassword'] = $errorPassword;
                         $check = 0;
@@ -44,7 +55,12 @@ class Login extends Controller
             else{
                 if($this->verifyLogin($emailOrLoginInput)){
                     $_SESSION['emailOrLoginInput'] =  $emailOrLoginInput;
-                    if($emailOrLoginInput != $login){
+                    $userQuery = $db->prepare('SELECT id, password FROM users WHERE login = :login');
+                    $userQuery->bindValue(':login', $emailOrLoginInput, PDO::PARAM_STR);
+                    $userQuery->execute();
+
+                    $ifUserExist = $userQuery->fetch(PDO::FETCH_ASSOC);
+                    if(!$ifUserExist){
                         $errorPassword = "*Podano błędny login lub hasło";
                         $_SESSION['errorPassword'] = $errorPassword;
                         $check = 0;
@@ -57,19 +73,22 @@ class Login extends Controller
                     $check = 0;
                 }
             }
-            if($this->verifyPassword($passwordInput)){
-                if($passwordInput != $password){
-                    $errorPassword = "*Podano błędny email lub hasło";
+            if($check == 1){
+                if($this->verifyPassword($passwordInput)){
+                    $hashPassword = hash('sha256', $passwordInput);
+                    if($hashPassword != $ifUserExist['password']){
+                        $errorPassword = "*Podano błędny email lub hasło";
+                        $serverError = true;
+                        $_SESSION['errorPassword'] = $errorPassword;
+                        $check = 0;
+                    }
+                }
+                else{
+                    $errorPassword = "*Dane dostarczone do serwera nie zgadzają się z danymi klienta";
                     $serverError = true;
                     $_SESSION['errorPassword'] = $errorPassword;
                     $check = 0;
                 }
-            }
-            else{
-                $errorPassword = "*Dane dostarczone do serwera nie zgadzają się z danymi klienta";
-                $serverError = true;
-                $_SESSION['errorPassword'] = $errorPassword;
-                $check = 0;
             }
 
             if($check == 1){
