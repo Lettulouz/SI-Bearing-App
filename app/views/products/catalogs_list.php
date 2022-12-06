016f96e0c5c13f67039f89b4e1c4cae9cdfb6352
Lettulouz <dominikpiskor1@wp.pl>
2022-12-06 19:43:29 +0100

    Added catalog list

--- app/views/products/catalogs_list.php
+++ app/views/products/catalogs_list.php
@@ -1,4 +1,34 @@
-<p>lista katalogów</p>
+<h1 class="text-muted headers-padding">Lista katalogów</h1>
+    <hr class="divider ">
+    <div class="headers-padding" style="padding-right: 15px;">
+        <?php if($data['catalogsArray']) {?>
+        <table class="table text-center">
+        <thead>
+        <tr>
+        <th>Lp.</th>
+        <th>Nazwa</th>
+        <th>Ilość rekordów</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php 
+        $catalogs = $data['catalogsArray'];
+        $i = 1;
+        foreach($catalogs as $catalog) 
+        {
+            echo 
+            "<tr>
+            <td>{$i}</td>
+            <td>{$catalog['name']}</td>
+            <td>{$catalog['amount']}</td>
+            </tr>";
+            $i++;
+        }
+        ?>
+        </tbody>
+        </table>
+        <?php } else {echo "Brak dodanych katalogów.";}?>
+    </div>
 
 <script>
     document.getElementById('content_collapse').classList.add('show');
