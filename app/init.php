1d492d2f3877f7ac1612b621efb98107b033d1c0
Lettulouz <dominikpiskor1@wp.pl>
2022-12-07 11:40:49 +0100

    Added test mailing function

--- app/init.php
+++ app/init.php
@@ -4,4 +4,12 @@ require_once 'core/config.php';
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 ?>
