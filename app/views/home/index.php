9df3888e1daac951024e7976ddcff12b74c83e50
Kornwaliusz <wojciech.j.a.stachura@gmail.com>
2022-12-12 22:48:55 +0100

    home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,8 +1,39 @@
 <?php include "header.php"; ?>
 <?php// include "menu.php"; ?>
 
-    <h1>Hello World</h1>
-    <br>
-    <?=$data['message']?>
+    <center> <h1>Firma Grontsmar</h1> </center>
+
+    <div class="container">
+        <!--Mega rollo kebeb z dotatkowym serem i frytkami-->
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://i.imgur.com/dMNzMwu.jpg"  alt="zdjęcie łożyska" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title"> nazwa łożyska z bazy danych </h5>
+                        <p class="card-text"> Opis łożyska z bazy danych</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://i.imgur.com/dMNzMwu.jpg"  alt="zdjęcie łożyska" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title"> nazwa łożyska z bazy danych </h5>
+                        <p class="card-text"> Opis łożyska z bazy danych</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://i.imgur.com/dMNzMwu.jpg"  alt="zdjęcie łożyska" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title"> nazwa łożyska z bazy danych </h5>
+                        <p class="card-text"> Opis łożyska z bazy danych</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php include dirname(__FILE__,2) . "/footer.php"; ?>
