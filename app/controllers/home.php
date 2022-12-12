3a9bafdf6c957302814459c51f5f00fb40327bb3
Kornwaliusz <wojciech.j.a.stachura@gmail.com>
2022-12-12 23:17:19 +0100

    home page v2

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,9 +1,26 @@
 <?php
 class Home extends Controller
 {
+    /*
     public function index(){
         $message = "Witaj świecie";
         $this->view('home/index', ['message' => $message]);
     }
+    */
+
+    public function index(){
+        require_once dirname(__FILE__,2) . '/core/database.php';
+
+        $query="SELECT i.name as itemName, m.name as manufacturerName, amount, price
+        FROM items i INNER JOIN manufactures m ON i.id_manufacturer=m.id";
+        $result = $db->query($query);
+        $result = $result->fetchAll(PDO::FETCH_ASSOC);
+        
+        $this->view('home/index', ['itemsArray'=>$result]);
+    }
+
+
+
 }
+
 ?>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,8 +3,48 @@
 
     <center> <h1>Firma Grontsmar</h1> </center>
 
+    <?php
+    $j = 0;
+    $three = 3;
+    $items = $data['itemsArray'];
+    echo "<div class='container'>";
+    echo "<div class='row'>";
+    foreach($items as $j => $item) 
+    {
+
+
+
+            echo "<div class='col-4'>
+                    <div class='card'>
+                        <img src='https://i.imgur.com/dMNzMwu.jpg'  alt='zdjęcie łożyska' class='card-img-top'>
+                        <div class='card-body'>
+                            <h5 class='card-title'> {$item['itemName']}</h5>
+                            <p class='card-text'> {$item['manufacturerName']}</p>
+                        </div>
+                    </div>
+                </div> ";
+        
+        
+
+
+    
+        $j++;
+            /*
+        echo 
+        "<tr>
+        <td>".$j."</td>
+        <td>{$item['itemName']}</td>
+        <td>{$item['manufacturerName']}</td>
+        <td>{$item['amount']}</td>
+        <td>{$item['price']} zł</td>
+        </tr>";
+        */
+    }
+    echo "</div>";
+    echo  "</div>";
+    ?>
+    <!--
     <div class="container">
-        <!--Mega rollo kebeb z dotatkowym serem i frytkami-->
         <div class="row">
             <div class="col-4">
                 <div class="card">
@@ -35,5 +75,5 @@
             </div>
         </div>
     </div>
-
+    -->
 <?php include dirname(__FILE__,2) . "/footer.php"; ?>
