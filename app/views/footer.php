0bbbf4ee290a9f844583591ab193d69d93cc054e
Szpachlarz <kl.kolakowska@gmail.com>
2022-12-13 21:41:28 +0100

    footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,4 +1,89 @@
+<!-- Footer -->
+<footer class="text-center mt-3 text-lg-start bg-light text-muted">
 
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto py-3">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-3">
+            Grontsmar
+          </h6>
+          <p>
+            Nasza pasja, twoje zadowolenie. Z Wami od 1993 roku.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto py-3">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-3">
+            Pomoc i kontakt
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">FAQ</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Metody płatności</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ciuf</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ciuf</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto py-3">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-3">
+            Ciuf
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Ciuf</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ciuf</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ciuf</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ciuf</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto py-3 mb-md-0">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-3">Kontakt</h6>
+          <p> Katowice, ul. Panewnicka 4</p>
+          <p>info@grontsmar.com</p>
+          <p> + 01 234 567 88</p>
+          <p> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="#/">Grontsmar.pl</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 
 </body>
 </html>
