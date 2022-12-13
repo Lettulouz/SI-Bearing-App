12fa2a5f4988e83914b1915d09cd7087cc7260b1
[Arek2017] <[agront@interia.pl]>
2022-12-13 21:18:56 +0100

    index menu

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,14 +1,27 @@
 <?php include "header.php"; ?>
 <?php// include "menu.php"; ?>
 
-    <center> <h1>Firma Grontsmar</h1> </center>
-    <?php
-    echo '<form method="POST" action ="" >';
-    echo "<input type='submit' value='Szukaj' />";
-    echo "<input type='text' name='search'/>";
+    
 
-
-    ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <center> <h1>Firma Grontsmar</h1> </center>
+                    <?php
+                        echo '<form method="POST" action ="" >';
+                        echo "<input type='submit' value='Szukaj' />";
+                        echo "<input type='text' name='search'/>";
+                        echo "<a href='shopping.php'><i class='bi bi-basket2'></i></a>";
+                        
+                    ?>
+                </div>
+            </div>
+        </div>
+    </nav>
 
     <?php
     $j = 0;
