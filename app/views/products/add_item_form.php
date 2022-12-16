02aae48a744ca8ff9b30baa288d4e44aab5ae2fa
Lettulouz <dominikpiskor1@wp.pl>
2022-12-16 20:58:43 +0100

    Minor changes in adding item

--- app/views/products/add_item_form.php
+++ app/views/products/add_item_form.php
@@ -59,10 +59,10 @@
             </div>   
             <div class="col-6 col-xl-4 border-end border-2 px-4">
                 <h4>Dodawanie atrybutów</h4>
-                <div id="show_item">
+                <div id="show_attr">
                 </div>
                 <div>
-                    <button class="btn btn-success" id="add_item_btn">Dodaj atrybut</button>
+                    <button class="btn btn-success" id="add_attr_btn">Dodaj atrybut</button>
                 </div>
                 <input type="text" style="display:none" id="attributes" name="attributes" data-attr='<?php echo json_encode($data['attributes']); ?>'>
             </div>          
@@ -86,17 +86,20 @@
     });
     possibleOptions.sort();
     var alreadyUsed = [];
-    var i=0;
+    var attrNum=0;
+    var descNum=0;
 	$(document).ready(function(){
-	    $("#add_item_btn").click(function(e){
+	    $("#add_attr_btn").click(function(e){
             e.preventDefault();
-            updateList();
-            tempPossibleOptions = possibleOptions.filter(ar => !alreadyUsed.find(rm => (rm === ar)));
-            i++;
+            alreadyUsed.push("");
+            tempPossibleOptions = possibleOptions;
+            //console.log(tempPossibleOptions);
+            attrNum++;
+            var input = '#attribute_name' + attrNum;
             var html = '';
             html+='<div class="row">';
             html+='<div class="col-12 col-md-5 mb-3">';
-            html+='<select class="select2 form-control" id="attribute_name' + i +  '" placeholder="Atrybut" aria-label="example-xl" onchange="updateList();">';
+            html+='<select class="select2 form-control selectattr" id="attribute_name' + attrNum +  '" aria-label="example-xl" onchange="updateAttrList();">';
             html+='<option>';
             html+='</option>';
             tempPossibleOptions.forEach((id) => {
@@ -107,38 +110,90 @@
             html+='</select>';
             html+='</div>';
             html+='<div class="col-8 col-md-5 mb-3">';
-            html+='<input type="text" id="attribute_value' + i + '" class="form-control required" placeholder="Wartość atrybutu" required onkeyup="enableSubmit()" autocomplete="off">';
+            html+='<div class="form-floating">';
+            html+='<input type="text" id="' + input + '" class="form-control required" placeholder="Wartość" required onkeyup="enableAttrSubmit()" autocomplete="off">';
+            html+='<label for="'+ input +'">Wartość</label>';
+            html+='</div>';
             html+='</div>';
             html+='<div class="col-4 col-md-2 mb-3 d-grid">';
-            html+='<button class="btn btn-danger remove_item_btn">-</button>';
+            html+='<button class="btn btn-danger remove_attr_btn">-</button>';
             html+='</div></div>';
-            $("#show_item").append(html)
-            if(i>=jsArr.length){
-                $("#add_item_btn").prop('disabled', true);
+            $("#show_attr").append(html)
+
+            var input = '#attribute_name' + attrNum;
+
+            $(input).select2({
+                width: '100%',
+                placeholder: 'Atrybut'
+            });
+
+            if(attrNum>=jsArr.length){
+                $("#add_attr_btn").prop('disabled', true);
             }
             else{
-                enableSubmit();
+                enableAttrSubmit();
             }
+            updateAttrList();
         });
 
-        $(document).on('click', '.remove_item_btn', function(e){ 
+        $(document).on('click', '.remove_attr_btn', function(e){ 
             e.preventDefault();
-            let row_item = $(this).parent().parent();
+            let row_attr = $(this).parent().parent();
             let tempRMV = $(this).parent().parent().find('select').attr('id');
             var getValue = "#";
                 getValue += tempRMV;
                 getValue +=' :selected';
             var attribute_name = $(getValue).text();
-            $(row_item).remove();
-            i--;
-            if(i==0){
-                $("#add_item_btn").prop('disabled', false);
+            $(row_attr).remove();
+            attrNum--;
+            if(attrNum==0){
+                $("#add_attr_btn").prop('disabled', false);
             }else{
-                enableSubmit();
+                enableAttrSubmit();
             }
             alreadyUsed = alreadyUsed.filter(e => e !== attribute_name);
+            updateAttrList();
 
         });
+
+    
+
+        // ---------------------------------- //
+
+        $("#add_description_btn").click(function(e){
+            e.preventDefault();
+            descNum++;
+            var html = '';
+            html+='<div class="row">';
+            html+='<textarea class="form-control " style="overflow:hidden;" id="description' + descNum + '" name="text" maxlength="1000" placeholder="Type in your message" rows="2" cols="5" onkeyup="test()" onkeydown="test1()" onclick="test1()"></textarea>';
+            html+='<span class="pull-right label label-default" id="count_message' + descNum + '">TESTs</span><br><br>';
+            html+='</div>';
+
+            $("#show_desc").append(html)
+            if(descNum>=jsArr.length){
+                $("#add_desc_btn").prop('disabled', true);
+            }
+            else{
+               // enableDescriptionsSubmit();
+            }
+
+            var text_max = 1000;
+
+            $('#count_message1').text('0 / ' + text_max );
+
+            var sch = $('#description1').prop('scrollHeight');
+            sch = sch+10;
+            $('#description1').attr('style', `resize:none; font-size: 18px; height:${sch}px; overflow:hidden;`);
+
+
+
+
+        });
+
+
+
+
+
 
         $("#itemSubmitRemote").click(function(e){
             for(var j=1; j<=i; j++){
@@ -160,25 +215,54 @@
         });
 	});
 
-    function updateList(){
-        let tempRMV = $("#show_item").find("select");
-        for(var i=1;i<=tempRMV.length;i++){
-            var input = "";
-            input +="#attribute_name";
-            input += i;
-            for(var j=0;j<alreadyUsed.length;j++){
-                $(input + ' option[value="' + alreadyUsed[j] + '"]').toggle();
-            }
+    function updateAttrList(){
+        let tempRMV = $("#show_attr").find("select");
+        console.log(tempRMV);
+        //console.log(alreadyUsed);
 
+        
+
+        for(var i=1;i<=tempRMV.length;i++){
+            var input = '#' + $(tempRMV[i-1]).attr('id');
+            
             var selection = $(input).find(":selected").val();
-            if(selection && !alreadyUsed.includes(selection)) alreadyUsed.push(selection);
+            if(selection && !alreadyUsed.includes(selection)) {
+                alreadyUsed[i-1] = selection;
+            }
+            //console.log(alreadyUsed);
+        }
+
+        for(var i=1;i<=tempRMV.length;i++){
+            var input = '#' + $(tempRMV[i-1]).attr('id');
+
+            for(var j=0;j<possibleOptions.length;j++){
+                // sprawdzić czy value znajduje się w already used
+                $(input + ' option[value="' + alreadyUsed[j] + '"]').attr('style' , "display:default");
+            }
+        }
+
+        
+        for(var i=1;i<=tempRMV.length;i++){
+            var input = '#' + $(tempRMV[i-1]).attr('id');
+
+            
+            for(var j=0;j<alreadyUsed.length;j++){
+                // sprawdzić czy value znajduje się w already used
+                $(input + ' option[value="' + alreadyUsed[j] + '"]').attr('style', 'display: none');
+            }
+           
+
+          //  $(input).trigger('change');
+
+            //console.log(alreadyUsed);
 
         }
+        
     }
 
-    function enableSubmit(){
+    function enableAttrSubmit(){
         let inputs = document.getElementsByClassName('required'); // Enter your class name for a required field, this should also be reflected within your form fields.
-        let btn = $('#add_item_btn');
+        let btn = $('#add_attr_btn');
         for (var i = 0; i < inputs.length; i++){
             let changedInput = inputs[i];
             if (changedInput.value.trim() == "" || changedInput.value == null){
@@ -190,8 +274,8 @@
             }
         }
         btn.prop('disabled', !isValid);
-        if(i>=jsArr.length){
-                $("#add_item_btn").prop('disabled', true);
+        if(attrNum>=jsArr.length){
+                $("#add_attr_btn").prop('disabled', true);
             }
     }
 </script>
@@ -206,4 +290,6 @@ $('#manufacturer').select2({
     width: '100%',
     placeholder: 'Wybierz producenta'
 });
+
+
 </script>
