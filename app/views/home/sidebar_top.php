d66f246813312411bee2e0dfa60d3e7d159233ea
Szpachlarz <kl.kolakowska@gmail.com>
2022-12-18 17:42:18 +0100

    sidebar (to be continued)

---
+++ app/views/home/sidebar_top.php
@@ -0,0 +1,57 @@
+<div class="offcanvas bg-light sidebar" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="sidebar" aria-labelledby="offcanvasDarkLabel">
+  <div class="offcanvas-body">
+    <nav class="navbar-light">
+      <ul class="navbar-nav">
+        <li class="my-2 small fw-bold text-uppercase text-decoration-none">Filtry</li>
+        <li class="my-3">
+          <hr class="dropdown divider">
+        </li>
+        <li>
+        <a class="text-muted small fw-bold text-uppercase text-decoration-none sidebar-link"
+         data-bs-toggle="collapse" role="button"  aria-controls="users_collapse" aria-expanded="false" id="users_collapse_btn">Producenci
+         <span class="bi bi-chevron-right right-icon ms-auto"></span>
+        </a>
+         <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="check" id="manufacturer1" value="optionpr1">
+            <label class="form-check-label" for="manufacturer">Producent1</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="check2" id="manufacturer2" value="optionpr2">
+            <label class="form-check-label" for="manufacturer">Producent2</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="check3" id="manufacturer3" value="optionpr3">
+            <label class="form-check-label" for="manufacturer">Producent3</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="check4" id="manufacturer4" value="optionpr4">
+            <label class="form-check-label" for="manufacturer">Producent4</label>
+        </div>
+          
+        </li>
+        <li class="my-3">
+          <hr class="dropdown divider">
+        </li>
+        <li>
+        <a class="text-muted small fw-bold text-uppercase text-decoration-none sidebar-link"
+         data-bs-toggle="collapse" id="content_collapse_btn" role="button" aria-expanded="false" aria-controls="content_collapse">Atrybuty
+         <span class="bi bi-chevron-right right-icon ms-auto"></span>
+        </a>
+         <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="check" id="attribute1" value="optionatr1">
+            <label class="form-check-label" for="attribute">attribute1</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="check" id="attribute2" value="optionatr2">
+            <label class="form-check-label" for="attribute">attribute2</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="check" id="attribute3" value="optionatr3">
+            <label class="form-check-label" for="attribute">attribute3</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="check" id="attribute4" value="optionatr4">
+            <label class="form-check-label" for="attribute">attribute4</label>
+        </div>
+        </li>
+      </ul>
