ce3997d000b53efad7c41fa2d763e40329c987b7
Lettulouz <dominikpiskor1@wp.pl>
2023-01-09 06:44:58 +0100

    Added page informations

---
+++ app/views/admin/edit_informations.php
@@ -0,0 +1,65 @@
+<?php
+include 'adm_nav.php';
+
+?>
+
+<div class="row mx-2">
+        <div class="col-6">
+            <h1 class="text-muted headers-padding ">Edycja informacji</h1>
+        </div>
+        <div class="col-6 align-self-center">
+            <button type="submit" id="informationsSubmitRemote" name="informationsSubmitRemote" class="btn btn-primary btn-lg float-end me-3" >Edytuj</button>
+        </div>
+    </div>
+</div>
+    <hr class="divider mt-0">
+<div class="container" style="max-width:720px;">
+    <form action="" id="addItemForm" method="POST" autocomplete="off" enctype="multipart/form-data"> 
+        <div class="row m-2">
+            <div class="col itemField1">
+                <div class="row m-2">
+                    <div class="col">
+                        <div class="form-floating ">
+                            <input type="text" class="form-control" id="nameInput" name="siteName" placeholder="Nazwa" 
+                            value="<?=$data['result']['sitename']?>">
+                            <label class="form-control-lg lg-custom" for="nameInput">Nazwa strony</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row m-2">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-12 col-sm-9 me-sm-0">
+                                <input class="form-control" type="file" name="formFile" id="formFile" accept="image/png" onchange="preview()">
+                            </div>
+                            <div class="col-12 col-sm-3 mt-3 mt-sm-0 ms-sm-0">
+                                <button id="deleteImageBtn" onclick="clearImage()" class="btn btn-danger col-12">-</button>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <img id="output" class="img-thumbnail mt-3" style="display:none; object-fit:cover"/>
+                        </div>
+                        <label style="color:darkgray">*Dodawany obraz musi mieć proporcje 1:1, aby wyświetlał się poprawnie</label>
+                    </div>
+                </div>
+            </div>   
+        </div>  
+        <button type="submit" id="informationsEditSubmit" name="informationsEditSubmit" class="btn btn-primary btn-lg float-end" style="display:none"></button>
+    </form> 
+</div>
+
+<script>
+    document.getElementById('users_collapse').classList.add('show');
+    document.getElementById('users_collapse_btn').setAttribute( 'aria-expanded', 'true' );
+    document.getElementById('users_collapse_btn').setAttribute( 'style', 'color:white !important' );
+    document.getElementById('ad_list').setAttribute( 'style', 'color:white !important' );
+
+    $("#informationsSubmitRemote").click(function(e){ 
+        $("#informationsEditSubmit").click();
+    });
+</script>
+
+<?php
+include 'adm_feet.php';
+
+?>
