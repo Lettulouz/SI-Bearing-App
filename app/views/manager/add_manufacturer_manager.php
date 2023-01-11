72125af1e91c305806cb21a8e27e567a53fdc93c
tentegess <tenagent2222@gmail.com>
2023-01-11 20:20:24 +0100

    manager panel

---
+++ app/views/manager/add_manufacturer_manager.php
@@ -0,0 +1,8 @@
+<?php
+include 'mng_nav.php';
+
+include dirname(__FILE__,2) . "/items/add_manufacturer_form.php"; 
+
+include 'mng_feet.php';
+
+?>
---
+++ app/views/manager/list_of_uncategorized_items.php
@@ -0,0 +1,11 @@
+<?php
+include 'mng_nav.php';
+
+?>
+
+<?php include dirname(__FILE__,2) . "/items/uncategorised_items_list.php"; ?>
+
+<?php
+include 'mng_feet.php';
+
+?>
